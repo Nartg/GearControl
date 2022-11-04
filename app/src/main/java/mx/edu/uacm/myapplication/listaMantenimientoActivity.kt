3fc0b5eba1c60cd408a5ba0b182da8ecfac32c8f
Nartg <gerardo.martinez.flores@estudiante.uacm.edu.mx>
2022-11-04 11:11:04 -0600

    first commit

---
+++ app/src/main/java/mx/edu/uacm/myapplication/listaMantenimientoActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.uacm.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class listaMantenimientoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_mantenimiento)
+    }
+}
